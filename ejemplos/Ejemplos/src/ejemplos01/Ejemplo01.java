b0a9168a6208445627ce926c35cadd95a0a952f4
Josuee16 <josuexpardoch@gmail.com>
2024-01-03 16:09:09 -0500

    Comentarios Funcion y Procedimientos

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -18,18 +18,42 @@ public class Ejemplo01 {
         // TODO code application logic here
         
         
-        String miCiudad = obtenerCiudadMayuscula("Loja");
-        System.out.printf("%s\n", miCiudad);
+        //String miCiudad = obtenerCiudadMayuscula("Loja");
+        //System.out.printf("%s\n", miCiudad);
+        obtenerMultiplicacion(7, 50); /*
+        aqui no retornamos la información debido a que es un procedimiento
+        */
+        String miMensaje = obtenerMultiplicacionDos(7, 50);/*
+        aqui se retorna la información debido a que es una función 
+        */
+        System.out.printf("%s\n",miMensaje);
+    }
+     public static String obtenerMultiplicacionDos(int tabla, int limite){
+        int mult;
+        String t = "";
+        for (int i = 1; i <= limite; i++) {
+            mult = tabla * i;
+            t = String.format("%s %d * %d = %d\n",t,tabla,i
+                    ,mult);   
+        }
+       return t;
     }
     
-    
-    public String obtenerCiudadMayuscula(String m){
+    public static String obtenerCiudadMayuscula(String m){
         String m2 = m.toUpperCase();
         return m2;
     }
     
+    
     public static void obtenerMultiplicacion(int tabla, int limite){
-        
+        int mult;
+        String t = "";
+        for (int i = 1; i <= limite; i++) {
+            mult = tabla * i;
+            t = String.format("%s %d * %d = %d\n",t,tabla,i
+                    ,mult);   
+        }
+        System.out.printf("%s\n",t);
     }
     
     public static String obtenerNombre(){
